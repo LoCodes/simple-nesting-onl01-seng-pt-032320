694b80402d2162c6924c33d2019dcc75c78dfa1b
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-14 01:06:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].to_a
+		 programmer_hash[:dennis_ritchie][:languages].to_set
 end
 
 def adding_matz
