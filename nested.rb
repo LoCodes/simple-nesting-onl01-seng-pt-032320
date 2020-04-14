6b8f5c0b2de47de1dd7a78854ec2dcf0f882b61a
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-14 01:13:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -97,7 +97,7 @@ def adding_matz
 		 programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
-		 #programmer_hash
+		 programmer_hash
 
 
 end
